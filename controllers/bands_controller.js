d642f4f348ca9444aeb4b2355e4b365d43037248
tnfo222 <tayforbes92@hotmail.com>
2022-08-28 14:24:05 -0400

    Added stages and events controllers

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -6,7 +6,9 @@ const { Band } = db
 // FIND ALL BANDS
 bands.get('/', async (req, res) => {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ ['available_start_time', 'ABC'] ]
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         res.status(500).json(error)
