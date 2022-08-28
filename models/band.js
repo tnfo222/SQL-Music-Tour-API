ebf26ce01dd4b73d557cc7a8aa29485d5f5e4ddc
tnfo222 <tayforbes92@hotmail.com>
2022-08-28 09:41:24 -0400

    Added migration

--- models/band.js
+++ models/band.js
@@ -1,15 +1,23 @@
-//DEPENDENCIES
-const { Sequelize, DataTypes, Model } = require('sequelize')
-const sequelize = new Sequelize(process.env.PG_URI)
-
-//MODEL
-class Band extends Model{}
-
-Band.init({
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Band extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Band.init({
     band_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement:true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
     },
     name: {
         type: DataTypes.STRING,
@@ -26,13 +34,13 @@ Band.init({
     end_time: {
         type: DataTypes.DATE,
         allowNull: false
-    },
-}, {
+    }
+  }, {
     sequelize,
     modelName: 'Band',
-    LableName: 'band',
+    tableName: 'bands',
     timestamps: false
-})
-
-//EXPORT
-module.exports = Band
+  })
+;
+  return Band;
+};
