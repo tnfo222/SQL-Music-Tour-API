1365888818e6f0619506f49220c9f3941e4cca3d
tnfo222 <tayforbes92@hotmail.com>
2022-09-03 01:48:28 -0400

    Changed relationship of band, event, and meetgreet models

--- models/event.js
+++ models/event.js
@@ -9,8 +9,12 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Stage, StageEvent }) {
+      Event.belongsToMany(Stage, {
+        foreignKey: "event_id",
+        as: "stages",
+        through: StageEvent
+      })
     }
   }
   Event.init({
