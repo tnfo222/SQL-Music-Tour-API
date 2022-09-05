3c5abdda4b0639d8ae1ff80782a36b18e351cce7
tnfo222 <tayforbes92@hotmail.com>
2022-09-05 10:15:26 -0400

    Fixed associated models

--- models/event.js
+++ models/event.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Stage, StageEvent }) {
+    static associate({ Stage, StageEvent, MeetGreet, SetTime }) {
       //stages
       Event.belongsToMany(Stage, {
         foreignKey: "event_id",
